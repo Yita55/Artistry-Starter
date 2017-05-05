f67370917aebf32e803adc49596d7d371b2d0762
kenneth <kenneth@yxunistar.com>
2017-05-05 14:28:50 +0800

    upload code

---
+++ Artistry/ArtistTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ArtistTableViewCell.swift
+//  Artistry
+//
+//  Created by unistar on 24/04/2017.
+//  Copyright © 2017 RayWenderlich. All rights reserved.
+//
+
+import UIKit
+
+class ArtistTableViewCell: UITableViewCell {
+
+  @IBOutlet weak var bioLabel: UILabel!
+  @IBOutlet weak var nameLabel: UILabel!
+  @IBOutlet weak var artistImageView: UIImageView!
+  
+}
---
+++ Artistry/WorkTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  WorkTableViewCell.swift
+//  Artistry
+//
+//  Created by unistar on 24/04/2017.
+//  Copyright © 2017 RayWenderlich. All rights reserved.
+//
+
+import UIKit
+
+class WorkTableViewCell: UITableViewCell {
+
+  @IBOutlet weak var workImageView: UIImageView!
+  @IBOutlet weak var workTitleLabel: UILabel!
+  @IBOutlet weak var moreInfoTextView: UITextView!
+  
+}
